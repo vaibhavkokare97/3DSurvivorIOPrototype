5592b71f05c702d801a75c050e54ed91d66f57e4
Vaibhav Kokare <vaibhavkokare97@gmail.com>
2024-11-09 23:01:15 +0530

    player authoring ecs

--- Assets/Scripts/Player/PlayerAuthoring.cs
+++ Assets/Scripts/Player/PlayerAuthoring.cs
@@ -3,13 +3,18 @@ using UnityEngine;
 
 internal class PlayerAuthoring : MonoBehaviour
 {
-    public Joystick Joystick;
+    public float moveSpeed = 5f;
 }
 
 class PlayerAuthoringBaker : Baker<PlayerAuthoring>
 {
     public override void Bake(PlayerAuthoring authoring)
     {
-        
+        Entity entity = GetEntity(TransformUsageFlags.Dynamic);
+
+        AddComponent(entity, new PlayerComponent
+        {
+            moveSpeed = authoring.moveSpeed
+        });
     }
 }
--- Assets/Scripts/Player/PlayerSystem.cs
+++ Assets/Scripts/Player/PlayerSystem.cs
@@ -1,8 +1,16 @@
 using Unity.Burst;
 using Unity.Entities;
+using Unity.Transforms;
+using Unity.Collections;
+using Unity.Mathematics;
 
-partial struct PlayerSystem : ISystem
+public partial struct PlayerSystem : ISystem
 {
+    private EntityManager _entityManager;
+
+    private Entity _playerEntity;
+    private PlayerComponent _playerComponent;
+
     [BurstCompile]
     public void OnCreate(ref SystemState state)
     {
@@ -12,7 +20,17 @@ partial struct PlayerSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        
+        _entityManager = state.EntityManager;
+        _playerEntity = SystemAPI.GetSingletonEntity<PlayerComponent>();
+        _playerComponent = _entityManager.GetComponentData<PlayerComponent>(_playerEntity);
+
+        Move(ref state);
+    }
+
+    private void Move(ref SystemState state)
+    {
+        LocalTransform playerTransform = _entityManager.GetComponentData<LocalTransform>(_playerEntity);
+        playerTransform.Position = new float3();
     }
 
     [BurstCompile]
