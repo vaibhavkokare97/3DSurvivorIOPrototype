fdc8bcdfd8587314043d1fa44c407e1a378c8557
Vaibhav Kokare <vaibhavkokare97@gmail.com>
2024-11-11 10:52:00 +0530

    update game UI logic for restart

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -1,12 +1,19 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
     public static UIManager Instance { get; private set; }
 
+    [Header("COINS")]
     [SerializeField] private TextMeshProUGUI _coinText;
+
+    [Header("GAME OVER")]
+    [SerializeField] private RectTransform _gameOverPanel;
+    [SerializeField] private Button _gameRestartButton;
 
     private void Awake()
     {
@@ -20,11 +27,18 @@ public class UIManager : MonoBehaviour
     {
         GameManager.Instance.onCoinCountChange += ChangeCoinUIText;
         GameManager.Instance.OnHealthChange += ChangeHealthBar;
+
+        _gameRestartButton.onClick.AddListener(() => RestartGame());
+    }
+
+    private void RestartGame()
+    {
+        SceneManager.LoadScene("Game");
     }
 
     void Start()
     {
-        
+
     }
 
     void ChangeCoinUIText(int coinsValue)
@@ -34,7 +48,7 @@ public class UIManager : MonoBehaviour
 
     void ChangeHealthBar(float healthValue)
     {
-
+        _gameOverPanel.gameObject.SetActive(healthValue <= 0);
     }
 
     private void OnDisable()
